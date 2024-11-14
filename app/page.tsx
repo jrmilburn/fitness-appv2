1eeba49cceaae498af4685de81ffd35fd9febb6a
Joe Milburn <jrmilburn@outlook.com>
2024-11-14 15:56:20 +1000

    Update grid

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,12 @@ export default async function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="grid grid-cols-2 grid-rows-2 w-full h-full gap-4">
         <div className="w-full h-full">
-          <DashboardProgram />
-        </div>
-        <div className="w-full h-full">
           <DashboardWorkout />
         </div>
         <div className="w-full h-full">
+          <DashboardProgram />
+        </div>
+        <div className="col-span-2 w-full h-full">
           <DashboardUser />
         </div>
       </div>
