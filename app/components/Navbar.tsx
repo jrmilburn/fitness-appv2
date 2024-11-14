77a5d21270175f6993d676aaa0a8d44409b3d634
Joe Milburn <jrmilburn@outlook.com>
2024-11-14 14:03:37 +1000

    Add link to navbar logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -65,10 +65,12 @@ export default function Navbar() {
     <nav className="h-full border-r bg-background shadow-lg min-w-80">
       <ul className="flex flex-col justify-between p-8 h-screen max-h-[100%] items-left">
         {/* Logo */}
-        <div className="w-full">
-          <Image src="/logo.jpg" alt="App logo" width={80} height={80} className="mx-auto" />
-          <h2 className="font-bold text-3xl">JFit</h2>
-        </div>
+        <Link href='/'>
+          <div className="w-full flex flex-col items-center">
+            <Image src="/logo.jpg" alt="App logo" width={80} height={80} className="mx-auto" />
+            <h2 className="font-bold text-3xl">JFit</h2>
+          </div>
+        </Link>
 
         {/* Logged Out State */}
         {!session ? (
