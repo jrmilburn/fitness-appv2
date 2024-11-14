9ac982c4e922281415b4cbd24f05edef57dc59d2
Joe Milburn <jrmilburn@outlook.com>
2024-11-14 16:08:52 +1000

    Change row sizes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import DashboardUser from "./components/Dashboard/User";
 
 export default async function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[1fr_1fr] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="grid grid-cols-2 grid-rows-2 w-full h-full gap-4">
         <div className="w-full h-full">
           <DashboardWorkout />
