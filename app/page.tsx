902d502af70f162bbc5285f8638abc796d2683c4
Joe Milburn <jrmilburn@outlook.com>
2024-11-14 15:44:33 +1000

    Change dashboard grid items height

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,16 @@ import DashboardWorkout from "./components/Dashboard/CurrentWorkout";
 
 export default async function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center h-full p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="grid grid-cols-2 grid-rows-2 w-full h-full gap-4">
-        <DashboardProgram />
-        <DashboardWorkout />
-        <div className="bg-gray-400">Item 3</div>
-        <div className="bg-gray-500">Item 4</div>
+        <div className="w-full h-full">
+          <DashboardProgram />
+        </div>
+        <div className="w-full h-full">
+          <DashboardWorkout />
+        </div>
+        <div className="bg-gray-400 w-full h-full">Item 3</div>
+        <div className="bg-gray-500 w-full h-full">Item 4</div>
       </div>
     </div>
   );
