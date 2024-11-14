172577e968848c9cdb6219ff29d46fb89fb09889
Joe Milburn <jrmilburn@outlook.com>
2024-11-14 11:05:33 +1000

    Accidentally changed excercise component on last commit

--- app/workouts/current/page.tsx
+++ app/workouts/current/page.tsx
@@ -2,14 +2,14 @@
 
 import { useState, useEffect } from 'react';
 import { useRouter } from "next/navigation";
+import Link from 'next/link'
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
-import Excercise from '@/app/components/CurrentWorkout/Excercise';
+import Exercise from '@/app/components/CurrentWorkout/Excercise';
 import WorkoutHeader from '@/app/components/CurrentWorkout/WorkoutHeader';
 import CompleteWorkout from '@/app/components/CurrentWorkout/CompleteWorkout';
 
 export default function Workout() {
-
     interface Exercise {
         id: string;
         name: string;
@@ -32,12 +32,7 @@ export default function Workout() {
     const router = useRouter();
 
     useEffect(() => {
-        // Refresh the router to ensure we have the latest data
         router.refresh();
-
-        console.log(process.env.NEXT_PUBLIC_BASE_URL!);
-
-        // Fetch the most recent workout data after refreshing
         fetch(`${process.env.NEXT_PUBLIC_BASE_URL!}/api/workouts/current`, {
             method: 'GET',
             headers: {
@@ -46,7 +41,7 @@ export default function Workout() {
         })
         .then((response) => {
             if (!response.ok) {
-                setNoWorkout(true); // Trigger noWorkout state if the response is not OK
+                setNoWorkout(true);
                 return null;
             }
             return response.json();
@@ -55,17 +50,16 @@ export default function Workout() {
             if (data) {
                 setWorkout(data);
             }
-            setIsLoading(false); // Stop loading after data is fetched
+            setIsLoading(false);
         })
         .catch((error) => {
             console.error('Error fetching workout:', error);
             setNoWorkout(true);
-            setIsLoading(false); // Stop loading if there’s an error
+            setIsLoading(false);
         });
-    }, [router]); // Only run on initial mount
+    }, [router]);
 
     useEffect(() => {
-        // Check if all exercises are completed
         if (workout?.excercises) {
             const allCompleted = workout.excercises.every(excercise => excercise.completed === true);
             setCompleted(allCompleted);
@@ -76,7 +70,19 @@ export default function Workout() {
         return (
             <div className='mx-auto my-auto flex flex-col items-center space-y-16 overflow-y-scroll h-screen w-full p-8'>
                 <h2>No Workout Available</h2>
-                <p>Please Create a Program to Start a Workout</p>
+                <p>Please Create or Find a Program to Start a Workout</p>
+                <div className='flex'>
+                <button className='bg-black rounded-lg text-white font-bold'>
+                    <Link href='/create-program'>
+                        Create
+                    </Link>
+                </button>
+                <button className='bg-black rounded-lg text-white font-bold'>
+                    <Link href='/search'>
+                        Search
+                    </Link>
+                </button>
+                </div>
             </div>
         );
     }
@@ -99,11 +105,35 @@ export default function Workout() {
             {/* Exercise List */}
             {isLoading ? (
                 Array(3).fill(null).map((_, index) => (
-                    <Skeleton key={index} height={100} width="100%" />
+                    <div key={index} className="w-[100%] max-w-2xl mx-auto p-8 flex flex-col border-2 border-gray-200">
+                        <div className="flex justify-between items-center">
+                            <div className="flex flex-col">
+                                <Skeleton width={100} height={16} className="mb-2" /> {/* Muscle Group */}
+                                <Skeleton width={150} height={24} /> {/* Exercise Name */}
+                            </div>
+                            <div className="flex flex-col space-y-4 items-center">
+                                <Skeleton circle={true} height={24} width={24} /> {/* Info Icon */}
+                                <Skeleton circle={true} height={24} width={24} /> {/* Edit Icon */}
+                            </div>
+                        </div>
+
+                        <div className="w-[60%] flex justify-between mx-8 my-2">
+                            <Skeleton width={50} height={16} /> {/* Weight Label */}
+                            <Skeleton width={50} height={16} /> {/* Reps Label */}
+                        </div>
+
+                        {/* Set placeholders */}
+                        {Array(3).fill(null).map((_, i) => (
+                            <div key={i} className="w-full flex justify-between mx-8 my-2">
+                                <Skeleton width={80} height={20} /> {/* Set Weight */}
+                                <Skeleton width={40} height={20} /> {/* Set Reps */}
+                            </div>
+                        ))}
+                    </div>
                 ))
             ) : (
                 workout?.excercises && workout.excercises.map(excercise => (
-                    <Excercise 
+                    <Exercise 
                         key={excercise.id}
                         excercise={excercise}
                         weekRir={week?.repsInReserve}
