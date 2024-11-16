8a6b7843317bd24579c5c69ed6698d63c9ca1bf0
Joe Milburn <jrmilburn@outlook.com>
2024-11-16 11:18:46 +1000

    Add delete client

--- app/components/Coaching/ClientList.tsx
+++ app/components/Coaching/ClientList.tsx
@@ -1,17 +1,36 @@
 "use client"
 
+import { useState } from "react";
 import ClientTab from "./ClientTab"
 
 export default function ClientList({ clients }) {
+
+    const [initialClients, setInitialClients] = useState(clients);
+
+    const handleDeleteClient = async (id) => {
+        try {
+          const response = await fetch(`/api/clients/${id}`, {
+            method: 'DELETE',
+          });
+          if (response.ok) {
+            setInitialClients((prevClients) => prevClients.filter((client) => client.id !== id));
+          } else {
+            console.error('Failed to delete client');
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      };
 
     return (
 
         <div className="flex flex-col max-w-3xl mx-auto">
             <h2 className="my-4 text-3xl">Clients</h2>
-            {clients.map((client) => (
+            {initialClients.map((client) => (
                 <ClientTab 
                     key={client.id}
                     client={client}
+                    onDeleteClient={handleDeleteClient}
                 />
             ))}
         </div>
