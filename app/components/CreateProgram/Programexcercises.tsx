e2a9aecb073fedabf980aa8b02cde14403b17edc
Joe Milburn <jrmilburn@outlook.com>
2024-11-30 13:16:55 +1000

    Improve mobile program builder

--- app/components/CreateProgram/Programexcercises.tsx
+++ app/components/CreateProgram/Programexcercises.tsx
@@ -5,173 +5,171 @@ import { useRouter } from "next/navigation";
 import { useState } from 'react';
 import Loader from '../Loader';
 
+export default function ProgramExercises({ program, setProgram, type, onPrevious }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [advanced, setAdvanced] = useState(false);
+  const router = useRouter();
 
-export default function ProgramExcercises({ program, setProgram, type, onPrevious }) {
+  const handleCreate = async () => {
+    setIsLoading(true);
+    console.log('CREATED PROGRAM: ', program);
 
-    const [isLoading, setIsLoading] = useState(false);
-    const [advanced, setAdvanced] = useState(false);
-    const router = useRouter();
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL!}/api/program`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(program),
+    });
 
-    const handleCreate = async () => {
+    setIsLoading(false);
 
-        setIsLoading(true);
-
-        console.log('CREATED PROGRAM: ', program);
-        
-        const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL!}/api/program`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(program),
-        });
-
-        setIsLoading(false);
-
-        if(response.ok) {
-            router.push('/workouts/current');
-        }
-            
-    };
-
-    const handleSave = async () => {
-
-        setIsLoading(true);
-
-        const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/program/save`, {
-            method: 'POST',
-            headers: {
-                'Content-Type' : 'application/json'
-            },
-            body: JSON.stringify(program),
-        })
-
-        setIsLoading(false);
-
-        if(response.ok) {
-            router.push('/programs');
-        }
-
+    if (response.ok) {
+      router.push('/workouts/current');
     }
+  };
 
-    const toggleAdvanced = (e) => {
+  const handleSave = async () => {
+    setIsLoading(true);
 
-        e.preventDefault();
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/program/save`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(program),
+    });
 
-        const newAdvanced = !advanced;
+    setIsLoading(false);
 
-        setAdvanced(newAdvanced);
+    if (response.ok) {
+      router.push('/programs');
     }
+  };
 
-    return (
-        <>
+  const toggleAdvanced = (e) => {
+    e.preventDefault();
+    const newAdvanced = !advanced;
+    setAdvanced(newAdvanced);
+  };
 
-            {isLoading && (
-                <div className="fixed inset-0 bg-black/50 z-50 backdrop-blur-md flex flex-col h-screen items-center justify-center">
-                    <div className="text-white text-lg flex items-center jusityf-center">
-                        <span className='text-3xl font-bold'>Generating program</span>
-                    </div>
-                    <div className='flex items-center justify-center gap-2'>
-                        <Loader 
-                            size={10}
-                            color="#fff"/>
-                        <span className='text-white'>This could take a few minutes...</span>
-                    </div>
-                </div>
-            )}
+  return (
+    <>
+      {isLoading && (
+        <div className="fixed inset-0 bg-black/50 z-50 backdrop-blur-md flex flex-col items-center justify-center">
+          <div className="text-white text-lg flex items-center justify-center mb-4">
+            <span className="text-3xl font-bold">Generating program</span>
+          </div>
+          <div className="flex items-center justify-center gap-2">
+            <Loader size={10} color="#fff" />
+            <span className="text-white">This could take a few minutes...</span>
+          </div>
+        </div>
+      )}
 
-            <form className="w-[100%] h-[75%] sm:h-screen p-4 bg-background shadow-md rounded overflow-x-auto border-2">
-                <div className='flex gap-8 items-center'>
-                    <h2 className="text-3xl text-primary-text"><strong>{program.name}</strong> Exercises</h2>
-                    <button 
-                        className="inter-bold border-2 text-primary-text border-border p-2 hover:bg-highlight rounded flex gap-2"
-                        onClick={toggleAdvanced}>{advanced ? 'Switch to Basic Builder' : 'Switch to Advanced Builder'}</button>
-                </div>
+      <form className="w-full p-4 bg-background shadow-md rounded border-2">
+        {/* Disclaimer Message */}
+        <div className="mb-4">
+          <p className="text-sm text-secondary-text text-center md:hidden">
+            For the best experience, we recommend using the program builder on a desktop device.
+          </p>
+        </div>
 
-                {/* Workouts Container */}
-                <div className="w-full flex flex-col">
-                  <div className="flex space-x-4 w-full p-4 items-start">
-                  {(program.weeks.find(week => week.weekNo === 1) || program.weeks[0]).workouts.map((workout, index) => (
-                      <Workout
-                        key={`${workout.weekNo}-${index}`}
-                        workoutIndex={index}
-                        workout={workout.name}
-                        setProgram={setProgram}
-                        program={program}
-                        excercises={program.weeks[0].workouts[index].excercises}
-                        advanced={advanced}
-                      />
-                    ))}
-                  </div>
-                
-                  {/* Program Notes Section */}
-                  <div className="w-full px-4 my-6">
-                    <label htmlFor="program-notes" className="block text-primary-text inter-bold mb-2">
-                      Program Notes:
-                    </label>
-                    <textarea
-                      id="program-notes"
-                      className="w-full h-32 p-3 border border-gray-300 rounded-md resize-none focus:outline-none focus:ring-2 focus:ring-highlight"
-                      placeholder="Enter any notes or comments about the program here..."
-                      rows={10}
-                    />
-                  </div>
-                </div>
+        <div className="flex flex-col sm:flex-row items-center sm:gap-8">
+          <h2 className="text-2xl sm:text-3xl text-primary-text my-4">
+            <strong>{program.name}</strong> Exercises
+          </h2>
+          <button
+            className="inter-bold border-2 text-primary-text border-border px-4 py-2 hover:bg-highlight rounded flex gap-2 mt-4 sm:mt-0"
+            onClick={toggleAdvanced}
+          >
+            {advanced ? 'Switch to Basic Builder' : 'Switch to Advanced Builder'}
+          </button>
+        </div>
 
-                {type === 0 ? (
-                    <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 flex flex-col sm:flex-row sm:space-x-4 justify-center items-center scale-75 sm:scale-100">
-                        <button
-                            type="button"
-                            onClick={handleCreate}
-                            className="px-6 py-3 bg-black text-white font-semibold rounded-lg hover:bg-gray-900 transition-all duration-300 mb-2 sm:mb-0"
-                        >
-                            Create Program
-                        </button>
-                        <button
-                            type="button"
-                            onClick={handleSave}
-                            className="px-6 py-3 bg-black text-white font-semibold rounded-lg hover:bg-gray-900 transition-all duration-300 mb-2 sm:mb-0"
-                        >
-                            Save Program
-                        </button>
-                        <button
-                            type="button"
-                            onClick={onPrevious}
-                            className="px-6 py-3 bg-black text-white font-semibold rounded-lg hover:bg-gray-900 transition-all duration-300"
-                        >
-                            Back
-                        </button>
-                    </div>
-                ) : type === 1 ? (
-                    <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 flex flex-col sm:flex-row sm:space-x-4 justify-center items-center">
-                        <button
-                            type="button"
-                            onClick={handleCreate}
-                            className="px-6 py-3 bg-black text-white font-semibold rounded-lg hover:bg-gray-900 transition-all duration-300 mb-2 sm:mb-0"
-                        >
-                            Copy Program
-                        </button>
-                        <button
-                            type="button"
-                            onClick={handleSave}
-                            className="px-6 py-3 bg-black text-white font-semibold rounded-lg hover:bg-gray-900 transition-all duration-300"
-                        >
-                            Save Program
-                        </button>
-                    </div>
-                ) : (
-                    <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 flex justify-center items-center">
-                        <button
-                            type="button"
-                            onClick={handleCreate}
-                            className="px-6 py-3 bg-black text-white font-semibold rounded-lg hover:bg-gray-900 transition-all duration-300"
-                        >
-                            Assign Program
-                        </button>
-                    </div>
-                )}
+        {/* Workouts Container */}
+        <div className="w-full flex flex-col">
+          <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 w-full p-4 items-start max-w-[400px]">
+            {(program.weeks.find(week => week.weekNo === 1) || program.weeks[0]).workouts.map((workout, index) => (
+              <Workout
+                key={`${workout.weekNo}-${index}`}
+                workoutIndex={index}
+                workout={workout.name}
+                setProgram={setProgram}
+                program={program}
+                excercises={program.weeks[0].workouts[index].excercises}
+                advanced={advanced}
+              />
+            ))}
+          </div>
 
-            </form>
-        </>
-    );
+          {/* Program Notes Section */}
+          <div className="w-full px-2 sm:px-4 my-4">
+            <label htmlFor="program-notes" className="block text-primary-text inter-bold mb-2">
+              Program Notes:
+            </label>
+            <textarea
+              id="program-notes"
+              className="w-full h-32 p-3 border border-gray-300 rounded-md resize-none focus:outline-none focus:ring-2 focus:ring-highlight"
+              placeholder="Enter any notes or comments about the program here..."
+              rows={10}
+            />
+          </div>
+        </div>
+
+        {type === 0 ? (
+          <div className="mt-4 flex flex-col sm:flex-row sm:space-x-4 justify-center items-center">
+            <button
+              type="button"
+              onClick={handleCreate}
+              className="w-full sm:w-auto px-6 py-3 bg-black text-white font-semibold rounded-lg hover:bg-gray-900 transition-all duration-300 mb-2 sm:mb-0"
+            >
+              Create Program
+            </button>
+            <button
+              type="button"
+              onClick={handleSave}
+              className="w-full sm:w-auto px-6 py-3 bg-black text-white font-semibold rounded-lg hover:bg-gray-900 transition-all duration-300 mb-2 sm:mb-0"
+            >
+              Save Program
+            </button>
+            <button
+              type="button"
+              onClick={onPrevious}
+              className="w-full sm:w-auto px-6 py-3 bg-black text-white font-semibold rounded-lg hover:bg-gray-900 transition-all duration-300"
+            >
+              Back
+            </button>
+          </div>
+        ) : type === 1 ? (
+          <div className="mt-4 flex flex-col sm:flex-row sm:space-x-4 justify-center items-center">
+            <button
+              type="button"
+              onClick={handleCreate}
+              className="w-full sm:w-auto px-6 py-3 bg-black text-white font-semibold rounded-lg hover:bg-gray-900 transition-all duration-300 mb-2 sm:mb-0"
+            >
+              Copy Program
+            </button>
+            <button
+              type="button"
+              onClick={handleSave}
+              className="w-full sm:w-auto px-6 py-3 bg-black text-white font-semibold rounded-lg hover:bg-gray-900 transition-all duration-300"
+            >
+              Save Program
+            </button>
+          </div>
+        ) : (
+          <div className="mt-4 flex justify-center items-center">
+            <button
+              type="button"
+              onClick={handleCreate}
+              className="w-full sm:w-auto px-6 py-3 bg-black text-white font-semibold rounded-lg hover:bg-gray-900 transition-all duration-300"
+            >
+              Assign Program
+            </button>
+          </div>
+        )}
+      </form>
+    </>
+  );
 }
--- app/components/CreateProgram/Workout.jsx
+++ app/components/CreateProgram/Workout.jsx
@@ -1,111 +1,109 @@
 import Musclegroups from './Musclegroups';
 import Excercise from './Excercise';
-import { useState} from 'react';
+import { useState } from 'react';
 import { handleMoveUp, handleMoveDown } from '@/app/lib/factories/programFactory';
 
 export default function Workout({ workout, setProgram, program, excercises, advanced, workoutIndex }) {
-    const [muscleGroupsShown, setMuscleGroupsShown] = useState(false);
-    const [workoutDetails, setWorkoutDetails] = useState(
-        excercises.map((excercise) => ({
-          muscleGroup: excercise.muscleGroup,
-          excercise,
-        }))
-      );
-      
-    const [muscleGroups, setMuscleGroups] = useState([]);
+  const [muscleGroupsShown, setMuscleGroupsShown] = useState(false);
+  const [workoutDetails, setWorkoutDetails] = useState(
+    excercises.map((excercise) => ({
+      muscleGroup: excercise.muscleGroup,
+      excercise,
+    }))
+  );
 
-    console.log('excercises from workout: ', excercises);
+  const [muscleGroups, setMuscleGroups] = useState([]);
 
-    const addMuscleGroup = (muscleGroup) => {
-        setWorkoutDetails((prevDetails) => [
-            ...prevDetails,
-            { muscleGroup, excercise: {
-                name: ''
-            } } // Add muscleGroup with no exercise initially
-        ]);
-        setMuscleGroupsShown(false);
-    };
+  const addMuscleGroup = (muscleGroup) => {
+    setWorkoutDetails((prevDetails) => [
+      ...prevDetails,
+      { muscleGroup, excercise: { name: '' } } // Add muscleGroup with no exercise initially
+    ]);
+    setMuscleGroupsShown(false);
+  };
 
-    const showMuscleGroups = (e) => {
-        e.preventDefault();
-        setMuscleGroupsShown(true);
-    };
+  const showMuscleGroups = (e) => {
+    e.preventDefault();
+    setMuscleGroupsShown(true);
+  };
 
-    // Function to remove a muscle group from the workout
-    const removeMuscleGroup = (index) => {
-        setWorkoutDetails((prevDetails) => prevDetails.filter((_, i) => i !== index));
-    };
+  // Function to remove a muscle group from the workout
+  const removeMuscleGroup = (index) => {
+    setWorkoutDetails((prevDetails) => prevDetails.filter((_, i) => i !== index));
+  };
 
-    const moveUp = (e, index) => {
-        e.preventDefault();
-        const newProgram = handleMoveUp(index, program, workoutIndex);
-        setProgram(newProgram);
-        setWorkoutDetails((prevDetails) => {
-            if (index <= 0) return prevDetails; // Can't move the first item up
-    
-            const updatedDetails = [...prevDetails];
-            [updatedDetails[index - 1], updatedDetails[index]] = [
-                updatedDetails[index],
-                updatedDetails[index - 1],
-            ];
-            return updatedDetails;
-        });
-    };
+  const moveUp = (e, index) => {
+    e.preventDefault();
+    const newProgram = handleMoveUp(index, program, workoutIndex);
+    setProgram(newProgram);
+    setWorkoutDetails((prevDetails) => {
+      if (index <= 0) return prevDetails; // Can't move the first item up
 
-    const moveDown = (e, index) => {
-        e.preventDefault();
-        const newProgram = handleMoveDown(index, program, workoutIndex, excercises);
-        setProgram(newProgram);
-        setWorkoutDetails((prevDetails) => {
-            if (index >= prevDetails.length - 1) return prevDetails; // Can't move the last item down
-    
-            const updatedDetails = [...prevDetails];
-            [updatedDetails[index], updatedDetails[index + 1]] = [
-                updatedDetails[index + 1],
-                updatedDetails[index],
-            ];
+      const updatedDetails = [...prevDetails];
+      [updatedDetails[index - 1], updatedDetails[index]] = [
+        updatedDetails[index],
+        updatedDetails[index - 1],
+      ];
+      return updatedDetails;
+    });
+  };
 
+  const moveDown = (e, index) => {
+    e.preventDefault();
+    const newProgram = handleMoveDown(index, program, workoutIndex, excercises);
+    setProgram(newProgram);
+    setWorkoutDetails((prevDetails) => {
+      if (index >= prevDetails.length - 1) return prevDetails; // Can't move the last item down
 
-            return updatedDetails;
-        });
-    };
+      const updatedDetails = [...prevDetails];
+      [updatedDetails[index], updatedDetails[index + 1]] = [
+        updatedDetails[index + 1],
+        updatedDetails[index],
+      ];
+      return updatedDetails;
+    });
+  };
 
-    return (
-        <>
-            <div className="bg-background min-w-64 p-4 flex flex-col space-y-4 justify">
-                <h2 className="text-lg text-primary-text">{workout}</h2>
-                <div className="flex flex-col space-y-4">
-                {workoutDetails.map((detail, index) => {
-                    console.log('detail: ', detail);
-                    return (
-                    <Excercise
-                        key={`${workout.name}-${index}`}
-                        excerciseindex={index}
-                        handleMoveUp={moveUp}
-                        handleMoveDown={moveDown}
-                        muscleId={detail.muscleGroup?.id || null}
-                        muscleName={detail.muscleGroup?.name || "Unspecified"}
-                        excercise={detail.excercise.name}
-                        setWorkoutDetails={setWorkoutDetails}
-                        setProgram={setProgram}
-                        workout={workout}
-                        onDelete={() => removeMuscleGroup(index)}
-                        advanced={advanced}
-                        progressionType={detail.excercise.progressionType || detail.excercise.setProgression}
-                    />
-                )})}
-            </div>
-                <button type="button" className="bg-background-secondary text-primary-text p-2" onClick={showMuscleGroups}>
-                    Add Muscle Group +
-                </button>
-            </div>
-            <Musclegroups
-                visible={muscleGroupsShown}
-                onClose={() => setMuscleGroupsShown(false)}
-                onAdd={(muscleGroup) => addMuscleGroup(muscleGroup)}
-                muscleGroups={muscleGroups}
-                setMuscleGroups={setMuscleGroups}
+  return (
+    <>
+      <div className="bg-background min-w-64 p-4 flex flex-col space-y-4 w-full">
+        <h2 className="text-lg text-primary-text mb-2">{workout}</h2>
+        <div className="flex flex-col space-y-4">
+          {workoutDetails.map((detail, index) => (
+            <Excercise
+              key={`${workout.name}-${index}`}
+              excerciseindex={index}
+              handleMoveUp={moveUp}
+              handleMoveDown={moveDown}
+              muscleId={detail.muscleGroup?.id || null}
+              muscleName={detail.muscleGroup?.name || "Unspecified"}
+              excercise={detail.excercise.name}
+              setWorkoutDetails={setWorkoutDetails}
+              setProgram={setProgram}
+              workout={workout}
+              onDelete={() => removeMuscleGroup(index)}
+              advanced={advanced}
+              progressionType={detail.excercise.progressionType || detail.excercise.setProgression}
             />
-        </>
-    );
+          ))}
+        </div>
+        <button
+          type="button"
+          className="w-full bg-background-secondary text-primary-text p-2 rounded-lg mt-4 hover:bg-highlight"
+          onClick={showMuscleGroups}
+        >
+          Add Muscle Group +
+        </button>
+      </div>
+      {muscleGroupsShown && (
+        <Musclegroups
+          visible={muscleGroupsShown}
+          onClose={() => setMuscleGroupsShown(false)}
+          onAdd={(muscleGroup) => addMuscleGroup(muscleGroup)}
+          muscleGroups={muscleGroups}
+          setMuscleGroups={setMuscleGroups}
+        />
+      )}
+    </>
+  );
 }
