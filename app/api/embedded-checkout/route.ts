4e92bc49b4f1707b8bc8b3bacdfc3e1bd4f88e53
Joe Milburn <jrmilburn@outlook.com>
2024-12-04 14:59:17 +1000

    update embed checkout metadata

--- app/api/embedded-checkout/route.ts
+++ app/api/embedded-checkout/route.ts
@@ -1,10 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/lib/authOptions";
+import { prisma } from "@/app/lib/prisma";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(request: NextRequest) {
   try {
+
+    const userSession = await getServerSession(authOptions);
+
+    if (!userSession) {
+      return NextResponse.json({ error: 'User not authenticated' }, { status: 401 });
+    }
+
+    const userEmail = userSession?.user.email;
+
+    const user = await prisma.user.findUnique({
+      where: { email: userEmail },
+    });
+
     const { priceId } = await request.json();
 
     const session = await stripe.checkout.sessions.create({
@@ -16,6 +32,9 @@ export async function POST(request: NextRequest) {
           quantity: 1
         }
       ],
+      metadata: {
+        userId: user.id 
+      },
       mode: 'subscription',
       return_url: `${request.headers.get('origin')}/return?session_id={CHECKOUT_SESSION_ID}`,
     })
