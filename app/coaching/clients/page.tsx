7bd88a04956159b70b90856d0c9ae4b452ee71b4
Joe Milburn <jrmilburn@outlook.com>
2024-12-05 14:46:04 +1000

    Change formatting of coacing page

--- app/coaching/clients/page.tsx
+++ app/coaching/clients/page.tsx
@@ -18,8 +18,10 @@ export default async function Clients() {
 
         return(
             <>
+                <div className="max-w-2xl mx-auto p-4">
                 <p>Coaching is only available to premium users.</p>
                 <StandardUserDetails />
+                </div>
             </>
         )
 
