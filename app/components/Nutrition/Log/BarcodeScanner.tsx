08afa991fa25991ae5e8e91b9750b825f44559c6
Joe Milburn <jrmilburn@outlook.com>
2024-12-15 17:54:18 +1000

    update spacing of scanner

--- app/components/Nutrition/Log/BarcodeScanner.tsx
+++ app/components/Nutrition/Log/BarcodeScanner.tsx
@@ -22,30 +22,36 @@ const BarcodeScanner = ({ onDetected, onError, onStop }) => {
   return (
     <div
       className="
-        fixed top-16 bottom-24 left-0 right-0 w-full
+        fixed top-16 bottom-20 left-0 right-0 w-full h-full
         border-2 border-dashed border-gray-300
         overflow-hidden
+        flex items-center justify-center
 
         sm:relative sm:top-auto sm:bottom-auto sm:left-auto sm:right-auto
         sm:mx-auto sm:my-16
-        sm:max-w-md sm:h-64
+        sm:w-full sm:h-full
       "
     >
       {isLoading && (
-        <div className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-50">
+        <div className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-50 z-10">
           <p className="text-primary-text">Initializing camera...</p>
         </div>
       )}
-      <BarcodeScannerComponent
-        onUpdate={handleUpdate}
-        height={'100%'}
-        width={"100%"}
-      />
+      
+      {/* Container to make BarcodeScannerComponent fill the parent */}
+      <div className="w-full h-full">
+        <BarcodeScannerComponent
+          onUpdate={handleUpdate}
+          height={'100%'}
+          width={"100%"}
+        />
+      </div>
+
       <button
         onClick={() => {
           if (onStop) onStop(); // Inform parent to stop scanning
         }}
-        className="mt-2 px-4 py-2 bg-red-500 text-white rounded absolute bottom-5 right-2 z-10"
+        className="mt-2 px-4 py-2 bg-red-500 text-white rounded absolute bottom-5 right-2 z-20"
         aria-label="Stop Scanning"
       >
         Stop Scanning
