3129c5682840cb88dc2d478fc9e624decce76425
Joe Milburn <jrmilburn@outlook.com>
2024-12-26 15:51:36 +1000

    log program templates

--- app/components/CreateProgram/Programtemplates.tsx
+++ app/components/CreateProgram/Programtemplates.tsx
@@ -47,99 +47,106 @@ export default function ProgramTemplates({ setProgram, onNext }) {
 
   return (
     <div className="flex flex-col w-full sm:max-w-screen-sm sm:mx-auto">
-      <h2 className="text-left text-2xl sm:text-3xl text-primary-text p-2 pb-4">JFIT Templates</h2>
-
+      <h2 className="text-left text-2xl sm:text-3xl text-primary-text p-2 pb-4">
+        JFIT Templates
+      </h2>
+  
       {isLoading ? (
         <>
-        <Skeleton height={100} width="100%" />
-        <Skeleton height={100} width="100%" />
-        <Skeleton height={100} width="100%" />
+          <Skeleton height={100} width="100%" />
+          <Skeleton height={100} width="100%" />
+          <Skeleton height={100} width="100%" />
         </>
-
       ) : (
-
-        programTemplates.map((program) => (
-          <div
-            key={program.id}
-            className={`w-full bg-background-secondary p-4 rounded border-b-2 border-border shadow-md hover:bg-background cursor-pointer`}
-            onClick={() => onSelect(program.id)}
-            
-          >
-            <div className="w-full flex justify-between items-start">
-              <div>
-                <h2 className="text-xl inter-bold text-primary-text">{program.name}</h2>
-  
-                <p className="text-sm text-secondary-text">
-                  {program.length} Weeks - {program.days} Days / Week
-                </p>
-                <p className="text-sm text-secondary-text">
-                  Created: {new Date(program.createdAt).toLocaleDateString()}
-                </p>
-              </div>
-              <div className="flex flex-col justify-center h-full space-y-2 items-center">
-              <button
-                onClick={(event) => {
-                  event.stopPropagation(); // Prevent the click from reaching parent div
-                  toggleSummary(program.id);
-                }}
-                className="text-primary-text p-2 rounded"
-              >
-                {expandedProgramId === program.id ? (
-                  <ChevronUpIcon className="h-6 w-6 text-primary-text hover:text-secondary-text transition-all duration-300" />
-                ) : (
-                  <ChevronUpIcon className="h-6 w-6 text-primary-text hover:text-secondary-text transition-all duration-300 rotate-180" />
-                )}
-              </button>
-              </div>
-            </div>
-  
-            {/* Summary Dropdown */}
+        programTemplates.map((program) => {
+          console.log(program);
+          return (
             <div
-              className={`transition-all duration-300 ease-in-out ${
-                expandedProgramId === program.id
-                  ? "opacity-100"
-                  : "max-h-0 opacity-0"
-              } overflow-hidden`}
+              key={program.id}
+              className="w-full bg-background-secondary p-4 rounded border-b-2 border-border shadow-md hover:bg-background cursor-pointer"
+              onClick={() => onSelect(program.id)}
             >
-              <div className="bg-background p-4 rounded shadow-inner mt-4">
-                <h3 className="text-lg inter-bold mb-2 text-primary-text">
-                  {program.name} - Weekly Summary
-                </h3>
-                {program.weeks[0]?.workouts?.length > 0 ? (
-                  <div className="space-y-4">
-                    {program.weeks[0].workouts.map((workout) => (
-                      <div
-                        key={workout.id}
-                        className="bg-background-secondary p-4 rounded shadow-sm"
-                      >
-                        <h4 className="text-md inter-bold text-primary-text">{workout.name}</h4>
-                        <ul className="list-disc pl-5 mt-2">
-                          {workout.excercises.map((excercise) => (
-                            <li key={excercise.id} className="text-primary-text">
-                              {excercise.name} <span className="text-secondary-text">({excercise.muscleGroup.name})</span>
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-                    ))}
-                  </div>
-                ) : (
-                  <p className="text-sm text-gray-500">
-                    No workouts found for Week 1.
+              <div className="w-full flex justify-between items-start">
+                <div>
+                  <h2 className="text-xl inter-bold text-primary-text">
+                    {program.name}
+                  </h2>
+                  <p className="text-sm text-secondary-text">
+                    {program.length} Weeks - {program.days} Days / Week
                   </p>
-                )}
-                <p className="text-secondary-text">{program.notes}</p>
+                  <p className="text-sm text-secondary-text">
+                    Created: {new Date(program.createdAt).toLocaleDateString()}
+                  </p>
+                </div>
+                <div className="flex flex-col justify-center h-full space-y-2 items-center">
+                  <button
+                    onClick={(event) => {
+                      event.stopPropagation(); // Prevent the click from reaching parent div
+                      toggleSummary(program.id);
+                    }}
+                    className="text-primary-text p-2 rounded"
+                  >
+                    {expandedProgramId === program.id ? (
+                      <ChevronUpIcon className="h-6 w-6 text-primary-text hover:text-secondary-text transition-all duration-300" />
+                    ) : (
+                      <ChevronUpIcon className="h-6 w-6 text-primary-text hover:text-secondary-text transition-all duration-300 rotate-180" />
+                    )}
+                  </button>
+                </div>
+              </div>
+  
+              {/* Summary Dropdown */}
+              <div
+                className={`transition-all duration-300 ease-in-out ${
+                  expandedProgramId === program.id
+                    ? "opacity-100"
+                    : "max-h-0 opacity-0"
+                } overflow-hidden`}
+              >
+                <div className="bg-background p-4 rounded shadow-inner mt-4">
+                  <h3 className="text-lg inter-bold mb-2 text-primary-text">
+                    {program.name} - Weekly Summary
+                  </h3>
+                  {program.weeks[0]?.workouts?.length > 0 ? (
+                    <div className="space-y-4">
+                      {program.weeks[0].workouts.map((workout) => (
+                        <div
+                          key={workout.id}
+                          className="bg-background-secondary p-4 rounded shadow-sm"
+                        >
+                          <h4 className="text-md inter-bold text-primary-text">
+                            {workout.name}
+                          </h4>
+                          <ul className="list-disc pl-5 mt-2">
+                            {workout.excercises.map((excercise) => (
+                              <li key={excercise.id} className="text-primary-text">
+                                {excercise.name}{" "}
+                                <span className="text-secondary-text">
+                                  ({excercise.muscleGroup.name})
+                                </span>
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                      ))}
+                    </div>
+                  ) : (
+                    <p className="text-sm text-gray-500">
+                      No workouts found for Week 1.
+                    </p>
+                  )}
+                  <p className="text-secondary-text">{program.notes}</p>
+                </div>
               </div>
             </div>
-          </div>
-        ))
-
+          );
+        })
       )}
-
+  
       {/* Render message if no templates are found */}
       {programTemplates.length === 0 && !isLoading && (
         <p className="text-gray-500">No JFIT templates could be found.</p>
       )}
     </div>
   );
-}
+}  
